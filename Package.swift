5f9dee96a57ddd10769dc7f04231f8d88525355d
StefanKScanbot <stefankrzhovski@scanbot.io>
2026-05-27 13:31:51 +0200

    Release 9.0.2

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ScanbotSDKNativeWrapper",
-            url: "https://download.scanbot.io/sdk/native-wrapper/ios/cocoapods/scanbot-sdk-native-wrapper-pods-9.0.1.zip",
-            checksum: "650e2d55595654654bfe297f1158a294439df5accf367a1e21680326f7c356d9"
+            url: "https://download.scanbot.io/sdk/native-wrapper/ios/cocoapods/scanbot-sdk-native-wrapper-pods-9.0.2.zip",
+            checksum: "a1b59046c8eda153fd386ac4e6f94fad8248f2f8b801b404500456ca1c011d66"
         ),
         .binaryTarget(
             name: "ScanbotSDK",
-            url: "https://download.scanbot.io/sdk/ios/pre/xcframeworks/RC9/scanbot-ios-sdk-xcframework-9.0.0.zip",
-            checksum: "eec14969879c2cf9f01c2ef6e28f57133931939f7fbce3d76da65bfa1e32f7bd"
-        ),
+            url: "https://download.scanbot.io/sdk/ios/pre/xcframeworks/RC10/scanbot-ios-sdk-xcframework-9.0.0.zip",
+            checksum: "fe84a2a9a89ba44f2ddb31929c8ff095166815a4b6583624ea49148bd16a43b7"
+        )
     ]
 )
